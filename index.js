50248f6fc72eda23a2e6a5e20d1705f57755bdda
Gabriela Goncalvez <gabriela.goncalvez@posteo.net>
2025-08-19 13:13:45 +0200

    feat: hello world app + Dockerfile + Cloud Build

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Hello World');
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World v1 🚀');
+});
+app.listen(process.env.PORT || 8080);
